9c3e2e1872778a92f73ddcc6237025099a0e904d
Jakub <jakub7zalewski7@gmail.com>
2023-08-27 22:02:09 +0200

    first commit

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,60 @@
+// @ts-nocheck
+("use strict");
+
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products } = ctx.request.body;
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::product.product")
+            .findOne(product.id);
+
+          const size = item.sizes.find((el) => el === product.size);
+          const color = item.colors.find(
+            (el) => el.name === product.color
+          ).name;
+
+          return {
+            price_data: {
+              currency: "pln",
+              product_data: {
+                name: `${item.title} ${item.schoolName} ${color} ${size}`,
+                metadata: {
+                  title: item.title,
+                  schoolName: item.schoolName,
+                  size: size,
+                  color: color,
+                },
+              },
+              unit_amount: Math.round(item.discountPrice * 100),
+            },
+            quantity: product.quantity,
+          };
+        })
+      );
+
+      const session = await stripe.checkout.sessions.create({
+        shipping_address_collection: { allowed_countries: [] },
+        payment_method_types: ["card", "blik", "p24"],
+        mode: "payment",
+        success_url: process.env.CLIENT_URL + "?success=true",
+        cancel_url: process.env.CLIENT_URL + "?success=false",
+        line_items: lineItems,
+      });
+
+      await strapi
+        .service("api::order.order")
+        .create({ data: { products, stripeId: session.id } });
+
+      return { stripeSession: session };
+    } catch (error) {
+      ctx.response.status = 500;
+      return { error };
+    }
+  },
+}));
